58182545207122bf0bdc8aa2fe667e9d0d9b92df
David Wybourn <david.wybourn@gmail.com>
2019-04-17 15:45:17 +0100

    NOJIRA Log event startup and add app_version environment variable.

--- api/src/main/java/uk/gov/dhsc/htbhf/card/CardServicesApplication.java
+++ api/src/main/java/uk/gov/dhsc/htbhf/card/CardServicesApplication.java
@@ -1,22 +1,49 @@
 package uk.gov.dhsc.htbhf.card;
 
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.event.EventListener;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import uk.gov.dhsc.htbhf.CommonRestConfiguration;
+import uk.gov.dhsc.htbhf.logging.EventLogger;
+import uk.gov.dhsc.htbhf.logging.LoggingConfiguration;
+import uk.gov.dhsc.htbhf.logging.event.ApplicationStartedEvent;
 
 /**
  * The starting point for spring boot, this class enables SpringFox for documenting the api using swagger
  * and defines a number of beans.
  * See also: {@link ApiDocumentation}.
  */
-@AllArgsConstructor
 @SpringBootApplication
 @EnableSwagger2
-@Import(CommonRestConfiguration.class)
+@Import({CommonRestConfiguration.class, LoggingConfiguration.class})
 public class CardServicesApplication {
+
+    @Value("${app.version:}") // use APP_VERSION env variable if available, otherwise give no version info
+    private String appVersion;
+
+    @Value("${instance.index:}") // use INSTANCE_INDEX env variable if available, otherwise give no index info
+    private String instanceIndex;
+
+    @Value("${vcap.application.application_id:}") // the id of the application as provided by cf
+    private String applicationId;
+
+    @Autowired
+    private EventLogger eventLogger;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void logAfterStartup() {
+        eventLogger.logEvent(ApplicationStartedEvent.builder()
+                .applicationId(applicationId)
+                .applicationVersion(appVersion)
+                .instanceIndex(instanceIndex)
+                .build()
+        );
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CardServicesApplication.class, args);
